2e2b6f6e127bb770956b9cc0b86a82fe916c18ac
MattCorb <32273733+MattCorb@users.noreply.github.com>
2022-03-28 10:51:55 -0600

    added auth and cicd

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "teachyoself708aba31": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
